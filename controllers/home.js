9d56e0aee6225310139377c2511d973bb5beec82
Keaton Okkonen <keaton.okkonen@gmail.com>
2015-12-02 23:21:05 +1100

    goodfaith init push

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,17 @@
-/**
- * GET /
- * Home page.
- */
+var User = require('../models/User');
+
 exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  });
+    // If user IS logged in
+    if (req.user) {
+        // console.log(user._id);
+        res.render('home', {
+            title: 'Home',
+            user: req.user
+        });
+    } else { // If user is NOT logged in
+        res.render('index', {
+            title: 'Index',
+        });
+    }
+
 };
